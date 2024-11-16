0cb09c0624cc7906b45b8696c8a995868683552f
stephen-waithira <137278161+stephen-waithira@users.noreply.github.com>
2024-11-16 15:07:28 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "7ubppc",
     baseUrl: "https://opensource-demo.orangehrmlive.com",
     supportFile: "cypress/support/commands.js"
   },
